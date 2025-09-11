5c16f2e5ad192b80d0e459fd73174969ada1dd81
Rafa130206 <rafabompadre@gmail.com>
2025-09-11 14:05:54 -0300

    Interface CalculadoraService criada

---
+++ cp4_GeoMasterAPI/Service/ICalculadoraService.cs
@@ -0,0 +1,10 @@
+﻿namespace cp4_GeoMasterAPI.Service
+{
+    public interface ICalculadoraService
+    {
+        double CalcularArea(object forma);
+        double CalcularPerimetro(object forma);
+        double CalcularVolume(object forma);
+        double CalcularAreaSuperficial(object forma);
+    }
+}
