9658d593c1300d0dbb3a73adf6ed4a2e0487620b
Rafa130206 <rafabompadre@gmail.com>
2025-09-11 14:24:07 -0300

    Service adicionado no build

--- cp4_GeoMasterAPI/Program.cs
+++ cp4_GeoMasterAPI/Program.cs
@@ -1,3 +1,6 @@
+using cp4_GeoMasterAPI.Service;
+using cp4_GeoMasterAPI.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<ICalculadoraService, CalculadoraService>();
 
 var app = builder.Build();
 
