e8fdebf7b6b28bc930bd566dbde4fc757b71319c
Rafa130206 <rafabompadre@gmail.com>
2025-09-11 14:15:20 -0300

    CalculadoraService criada

--- cp4_GeoMasterAPI/Interfaces/ICalculos3D.cs
+++ cp4_GeoMasterAPI/Interfaces/ICalculos3D.cs
@@ -1,6 +1,6 @@
 ﻿namespace cp4_GeoMasterAPI.Interfaces
 {
-    public interface ICaulculos3D
+    public interface ICalculos3D
     {
         double CalcularVolume();
         double CalcularAreaSuperficial();
---
+++ cp4_GeoMasterAPI/Service/CalculadoraService.cs
@@ -0,0 +1,40 @@
+﻿using cp4_GeoMasterAPI.Interfaces;
+
+namespace cp4_GeoMasterAPI.Service
+{
+    public class CalculadoraService : ICalculadoraService
+    {
+        public double CalcularArea(object forma)
+        {
+            if (forma is ICalculos2D f2d)
+                return f2d.CalcularArea();
+
+            throw new NotSupportedException($"A forma {forma.GetType().Name} não suporta cálculo de área.");
+        }
+
+        public double CalcularPerimetro(object forma)
+        {
+            if (forma is ICalculos2D f2d)
+                return f2d.CalcularPerimetro();
+
+            throw new NotSupportedException($"A forma {forma.GetType().Name} não suporta cálculo de perímetro.");
+        }
+
+        public double CalcularVolume(object forma)
+        {
+            if (forma is ICalculos3D f3d)
+                return f3d.CalcularVolume();
+
+            throw new NotSupportedException($"A forma {forma.GetType().Name} não suporta cálculo de volume.");
+        }
+
+        public double CalcularAreaSuperficial(object forma)
+        {
+            if (forma is ICalculos3D f3d)
+                return f3d.CalcularAreaSuperficial();
+
+            throw new NotSupportedException($"A forma {forma.GetType().Name} não suporta cálculo de área superficial.");
+        }
+    }
+
+}
